7a86b4c20e187adf18a84aba64879cf01898c9b8
chaimp <chaim10@outlook.com>
2018-08-01 18:28:15 +0300

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,78 @@
+import {Component, OnDestroy} from '@angular/core';
+import {DragulaService} from 'ng2-dragula';
+import {Subscription} from 'rxjs';
+
+let id = 0;
+
+class Task {
+
+  taskId: number;
+  constructor(public ownerName: string, public img: string) {
+    this.taskId = id++;
+
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnDestroy {
+
+  title = 'TaskIt';
+
+  boss = [
+    new Task('Boss', 'assets/images/boss.png'),
+    new Task('Boss', 'assets/images/boss.png'),
+    new Task('Boss', 'assets/images/boss.png'),
+    new Task('Boss', 'assets/images/boss.png'),
+  ];
+
+  wally = [];
+
+  dilbert = [
+    new Task('Dilbert', 'assets/images/dilbert.png'),
+    new Task('Dilbert', 'assets/images/dilbert.png'),
+  ];
+
+  alice = [];
+
+  subs = new Subscription();
+
+  constructor(private dragulaService: DragulaService) {
+
+    this.subs.add(dragulaService.dropModel('TASK')
+      .subscribe(({el, target, source, sourceModel, targetModel, item}) => {
+        item.img = 'assets/images/' + target.id + '.png';
+        item.ownerName = target.id.charAt(0).toUpperCase() + target.id.slice(1);
+        console.log('dropModel:');
+        console.log(el);
+        console.log(source);
+        console.log(target);
+        console.log(sourceModel);
+        console.log(targetModel);
+        console.log(item);
+
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+}
+
+//
+// function onDragStart(event, data) {
+//   event.dataTransfer.setData('data', data);
+// }
+//
+// function onDrop(event, data) {
+//   let dataTransfer = event.dataTransfer.getData('data');
+//   event.preventDefault();
+// }
+//
+// function allowDrop(event) {
+//   event.preventDefault();
+// }
